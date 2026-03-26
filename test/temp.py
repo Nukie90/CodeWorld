db8a3b83a5a67bc4167914959e7c207c11f97b03
Intutch Sribanyaranond <intutch.sbyrn@gmail.com>
2026-03-26 21:30:57 +0700

    test

--- test/temp.py
+++ test/temp.py
@@ -1,13 +1,2 @@
-def test():
-    #write some error code
-    a = 1
-    b = 2
-    
-    
-    for i in range(10):
-        if i == 5:
-            pass
-        else:
-            pass
-    while i < 10:
-        
+def invalid_syntax()
+    print("This is missing a colon and has incorrect indentation")
