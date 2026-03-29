b36895a3c956288a787429654413419f5408e344
Intutch Sribanyaranond <intutch.sbyrn@gmail.com>
2026-03-29 23:00:24 +0700

    bug payload

--- backend/app/js_plugin/server.js
+++ backend/app/js_plugin/server.js
@@ -168,7 +168,8 @@ async function getLintResults(code, filename) {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 function calculateMaintainabilityIndex(halsteadVolume, cyclomaticComplexity, loc) {
     if (loc <= 0) return 100.0;
